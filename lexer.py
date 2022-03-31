0f81ae45c1db7e7ed0dc04e6ea75030faee02fb1
alannxq <85573908+alannxq@users.noreply.github.com>
2022-03-31 23:31:44 +0100

    Update lexer.py

--- lexer.py
+++ lexer.py
@@ -1,7 +1,8 @@
 import os
+import sys
 import time
 
-with open("test.al", "r") as f:
+with open(sys.argv[1], "r") as f:
     file = f.read()
     file = file.split("\n")
 
@@ -16,21 +17,54 @@ with open("output.py", "w") as f:
     f.write("") ## clear the output file
     # although im sure that there is a better way
 
+start_indenting = False
 
 with open("output.py", "a+") as f:
-    f.write("import os\n\n")
+    f.write("import os\nimport time\n\n")
     for i in tokens:
         if len(i) > 0:
-            if i[0] == "#":
-                print(i, "   COMMENT")
+
+            #############
+            #  COMMENT  #
+            #############
+
+
+            if i[0][:2] == "//":
+                f.write(F"## {i[1:]}\n")
                 continue
+
+            ################
+            #  END INDENT  #
+            ################
+
+            if i[0][0] == "}":
+                print("END INDENT: ", i)
+                start_indenting = False ## stop indenting (for python)
+                f.write("\n")
+                continue
+
+            ############
+            #  INDENT  #
+            ############
+
+            if start_indenting == True:
+                print("INDENTING: ", i)
+                f.write("\t")
+
+            ##################
+            #  START INDENT  #
+            ##################
+
+            if i[-1] == "{":
+                print("START INDENT: ", i)
+                start_indenting = True
+                i = i[:-1] ## remove left bracket
 
             ###########
             #  PRINT  #
             ###########
 
             if i[0] == "show":
-                print(i, "   SHOW")
                 stm = i[1:]
                 if len(stm) > 1:
                     #shown = []#','.join(i[1:])
@@ -61,12 +95,10 @@ with open("output.py", "a+") as f:
                             final_statement.append(elm)
 
 
-                    print(final_statement)
                     shown = ''.join(final_statement)
 
                 else:
                     shown = ''.join(i[1:])
-                    shown = ''.join(final_statement)
 
                 f.write(f"print({shown})\n")
                 continue
@@ -76,12 +108,23 @@ with open("output.py", "a+") as f:
             ##############
 
             if i[0] == "var":
-                print(i, "   VAR")
-                statement = eval("".join(i[3:]))
+                if i[1] == "true" or i[1] == "false":
+                    if i[1] == "true":
+                        statement = bool(1)
+                    if i[1] == "false":
+                        statement = bool(0)
+                else:
+                    statement = eval("".join(i[3:]))
+
                 try:
                     statement = int(statement)
                 except:
                     statement = f"'{statement}'"
+
+                if statement == "true":
+                    statement = bool(1)
+                if statement == "false":
+                    statement = bool(0)
 
                 f.write(f"{i[1]} = {statement}\n")
                 continue
@@ -91,15 +134,15 @@ with open("output.py", "a+") as f:
             ###############
 
             if i[0] == "|":
-                print(i, "   FUNC")
-                f.write(f"def {i[1]}():\n\t")
+                f.write(f"def {i[1]}():\n")
                 continue
 
-            if i[0] == "while":
-                print(i, "   WHILE")
+            if i[0] == "while": ## 1 > 0 {
                 condition = i[1:]
+                start_indenting = True
+
                 final_condition = "".join(str(elm) for elm in condition)
-                f.write(f"while {str(final_condition)}:\n\t")
+                f.write(f"while {str(final_condition)}:\n")
                 continue
 
             ###################
@@ -107,7 +150,6 @@ with open("output.py", "a+") as f:
             ##################
 
             if i[0] == ">":
-                print(i, "   CALL FUNC")
                 f.write(f"{i[1]}()\n")
                 continue
 
@@ -117,7 +159,12 @@ with open("output.py", "a+") as f:
 
             if i[0] == ">>":
                 if i[1] == "clear":
-                    f.write("os.system('clear')\n")
+                    f.write("\nos.system('cls')\n")
+                    continue
+
+                if i[1] == "wait":
+                    sleep_time = int(i[2])
+                    f.write(f"\ntime.sleep({sleep_time})")
                     continue
 
             ##################
@@ -127,5 +174,14 @@ with open("output.py", "a+") as f:
             if i[0] == "?":
                 condition = i[1:]
                 final_condition = "".join(str(elm) for elm in condition)
-                f.write(f"if {str(final_condition)}:\n\t")
+                f.write(f"if {str(final_condition)}:\n")
                 continue
+
+            ###############
+            #  INCREMENT  #
+            ###############
+
+            if i[0] == "++":
+                f.write(f"{i[1]} += 1")
+
+os.system("py output.py")
